5534c398894e48f58452f86b44a916639b42b569
brunaguarizo <b.guarizo@hotmail.com>
2025-12-16 19:55:35 -0800

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <a
+                    href='mailto:b.guarizo@hotmail.com'
+                    className={styles.email}>
+                    b.guarizo@hotmail.com
+                </a>
+                <div className={styles.socialLinks}>
+                    <a
+                        href='https://www.linkedin.com/in/brunaguarizo'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className={styles.socialLink}>
+                        LinkedIn
+                    </a>
+                    <span className={styles.separator}>|</span>
+                    <a
+                        href='https://github.com/brunaguarizo'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className={styles.socialLink}>
+                        GitHub
+                    </a>
+                    <span className={styles.separator}>|</span>
+                    <a
+                        href='https://www.behance.net/bguarizo'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className={styles.socialLink}>
+                        Behance
+                    </a>
+                </div>
+                <p className={styles.copyright}>
+                    All rights reserved, ©{currentYear}
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
