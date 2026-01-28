ef12295fb9c220a1f02d01f52133e288024917c2
brunaguarizo <guarizob@gmail.com>
2026-01-28 13:09:42 -0800

    switch toggle fix

--- src/components/SwitchButton/SwitchButton.jsx
+++ src/components/SwitchButton/SwitchButton.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import styles from "./SwitchButton.module.css";
 
-const SwitchButton = ({ value, onChange, leftLabel = "design", rightLabel = "code" }) => {
+const SwitchButton = ({
+    value,
+    onChange,
+    leftLabel = "design",
+    rightLabel = "code",
+}) => {
     const handleClick = () => {
         onChange(value === leftLabel ? rightLabel : leftLabel);
-    };
-
-    const handleLabelClick = (label) => {
-        if (value !== label) {
-            onChange(label);
-        }
     };
 
     return (
@@ -17,41 +16,23 @@ const SwitchButton = ({ value, onChange, leftLabel = "design", rightLabel = "cod
             className={styles.switchContainer}
             onClick={handleClick}>
             <div className={styles.switchTrack}>
-                {/* Quando está em design, mostra code à direita */}
-                {value === leftLabel ? (
-                    <>
-                        <div className={styles.switchThumb}>
-                            <span className={styles.switchThumbLabel}>
-                                {leftLabel}
-                            </span>
-                        </div>
-                        <span
-                            className={`${styles.switchLabel} ${styles.switchLabelRight}`}
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                handleLabelClick(rightLabel);
-                            }}>
-                            {rightLabel}
-                        </span>
-                    </>
-                ) : (
-                    <>
-                        {/* Quando está em code, mostra design à esquerda */}
-                        <span
-                            className={`${styles.switchLabel} ${styles.switchLabelLeft}`}
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                handleLabelClick(leftLabel);
-                            }}>
-                            {leftLabel}
-                        </span>
-                        <div className={`${styles.switchThumb} ${styles.switchThumbRight}`}>
-                            <span className={styles.switchThumbLabel}>
-                                {rightLabel}
-                            </span>
-                        </div>
-                    </>
-                )}
+                <span
+                    className={`${styles.switchLabel} ${
+                        value === leftLabel ? styles.switchLabelActive : ""
+                    }`}>
+                    {leftLabel}
+                </span>
+                <div
+                    className={`${styles.switchThumb} ${
+                        value === rightLabel ? styles.switchThumbRight : ""
+                    }`}
+                />
+                <span
+                    className={`${styles.switchLabel} ${
+                        value === rightLabel ? styles.switchLabelActive : ""
+                    }`}>
+                    {rightLabel}
+                </span>
             </div>
         </div>
     );
